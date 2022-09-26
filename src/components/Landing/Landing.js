5eca5f544ad97b10eaa478c4d658940abd144d03
Andrea Monroy <andreamonroy@MacBook-Air-de-Andrea.local>
2022-09-26 13:55:05 -0500

    add validators

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,69 +1,87 @@
-import logo from "../../logo.svg";
-import "./Landing.css";
-import Input from "../Input";
-import Sliders from "../Sliders/Sliders";
-import Footer from "../Footer";
-import { useEffect, useRef, useState } from "react";
-import axios from "axios";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, Autoplay } from "swiper";
-import { Link } from "react-router-dom";
+import logo from '../../logo.svg';
+import './Landing.css';
+import Input from '../Input';
+import Sliders from '../Sliders/Sliders';
+import Footer from '../Footer';
+import { useEffect, useRef, useState } from 'react';
+import axios from 'axios';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Autoplay } from 'swiper';
+import { Link, useNavigate } from 'react-router-dom';
 function Landing() {
   const myRef = useRef(null);
   const formRef = useRef(null);
   const directForm = useRef(null);
   // form states
-  const [nombres, setNombres] = useState("");
-  const [paterno, setPaterno] = useState("");
-  const [materno, setMaterno] = useState("");
-  const [correo, setCorreo] = useState("");
-  const [celular, setCelular] = useState("");
-  const [dni, setDni] = useState("");
-  const [carrera, setCarrera] = useState("");
-  const [modalidad, setModalidad] = useState("");
-  const [acepta, setAcepta] = useState("");
+  const [nombres, setNombres] = useState('');
+  const [paterno, setPaterno] = useState('');
+  const [materno, setMaterno] = useState('');
+  const [correo, setCorreo] = useState('');
+  const [celular, setCelular] = useState('');
+  const [dni, setDni] = useState('');
+  const [carrera, setCarrera] = useState('');
+  const [modalidad, setModalidad] = useState('');
+  const [acepta, setAcepta] = useState('');
   const [openCarreras, setOpenCarreras] = useState(false);
   const [openModalidad, setOpenModalidad] = useState(false);
   const [form, setForm] = useState(false);
+  const [messageForm, setMessageForm] = useState('');
+  const navigate = useNavigate()
   const handleSubmit = (e) => {
     e.preventDefault();
-    const data = {
-      Nombres: nombres,
-      Paterno: paterno,
-      Materno: materno,
-      Correo: correo,
-      Celular: celular,
-      DNI: dni,
-      Carrera: carrera,
-      Modalidad: modalidad,
-      Aceptacion_politicas_privacidad: acepta,
-    };
+    if (
+      nombres &&
+      paterno &&
+      materno &&
+      correo &&
+      celular &&
+      dni &&
+      carrera &&
+      modalidad &&
+      acepta
+    ) {
+      const data = {
+        Nombres: nombres,
+        Paterno: paterno,
+        Materno: materno,
+        Correo: correo,
+        Celular: celular,
+        DNI: dni,
+        Carrera: carrera,
+        Modalidad: modalidad,
+        Aceptacion_politicas_privacidad: acepta,
+      };
 
-    axios
-      .post(
-        "https://sheet.best/api/sheets/af09fd96-e7a2-4dcf-84d0-c0f30a7b03ff",
-        data
-      )
-      .then((response) => {
-        console.log(response);
-        setNombres("");
-        setPaterno("");
-        setMaterno("");
-        setCorreo("");
-        setCelular("");
-        setDni("");
-        setCarrera("");
-        setModalidad("");
-        setAcepta("");
-      });
+      axios
+        .post(
+          'https://sheet.best/api/sheets/af09fd96-e7a2-4dcf-84d0-c0f30a7b03ff',
+          data
+        )
+        .then((response) => {
+          console.log(response);
+          setNombres('');
+          setPaterno('');
+          setMaterno('');
+          setCorreo('');
+          setCelular('');
+          setDni('');
+          setCarrera('');
+          setModalidad('');
+          setAcepta('');
+          setMessageForm('');
+          navigate('/gracias')
+        });
+    } else {
+      setMessageForm('Por favor, completa todos los campos');
+    }
   };
 
   const executeScroll = () => {
-    myRef.current?.scrollIntoView({ behavior: "smooth" });
+    myRef.current?.scrollIntoView({ behavior: 'smooth' });
     setForm(true);
   };
   const executeScrollMobile = () => {
-    formRef.current?.scrollIntoView({ behavior: "smooth" });
+    formRef.current?.scrollIntoView({ behavior: 'smooth' });
     setForm(true);
   };
 
@@ -72,19 +90,19 @@ function Landing() {
   };
 
   const saveFile = () => {
-    window.open("/files/politicas_privacidad.pdf");
+    window.open('/files/politicas_privacidad.pdf');
   };
 
   useEffect(() => {
-    document.getElementById("vid1").addEventListener(
-      "loadedmetadata",
+    document.getElementById('vid1').addEventListener(
+      'loadedmetadata',
       function () {
         this.currentTime = 4;
       },
       false
     );
-    document.getElementById("vid2").addEventListener(
-      "loadedmetadata",
+    document.getElementById('vid2').addEventListener(
+      'loadedmetadata',
       function () {
         this.currentTime = 4;
       },
@@ -171,7 +189,7 @@ function Landing() {
           <div className=" sm:w-2/5 sm:flex sm:flex-col sm:ml-20 mx-4 sm:mx-0 sm:pb-0 pb-4 sm:px-20 z-10 relative">
             <div
               className={`${
-                form ? "form" : ""
+                form ? 'form' : ''
               } bg-white p-4 sm:flex sm:flex-col space-y-4 h-fit rounded-2xl sm:w-[440px] w-fit mt-20`}
               ref={formRef}
             >
@@ -237,7 +255,7 @@ function Landing() {
                       type="text"
                       placeholder="Seleccione Carrera*"
                       onClick={() => setOpenCarreras(!openCarreras)}
-                      value={carrera.replace(/_/g, " ")}
+                      value={carrera.replace(/_/g, ' ')}
                     ></input>
                     {openCarreras && (
                       <div className="w-full absolute  max-w-[200px] z-50 overflow-scroll h-40">
@@ -245,7 +263,7 @@ function Landing() {
                           className="bg-white px-4 py-2 cursor-pointer text-sm hover:bg-gray-50 border border-lightBlue text-gray-800"
                           onClick={(e) => {
                             setOpenCarreras(false);
-                            setCarrera("Administración_de_datos");
+                            setCarrera('Administración_de_datos');
                           }}
                         >
                           Administración de datos
@@ -255,7 +273,7 @@ function Landing() {
                           onClick={(e) => {
                             setOpenCarreras(false);
                             setCarrera(
-                              "Administración_de_Redes_y_Comunicaciones"
+                              'Administración_de_Redes_y_Comunicaciones'
                             );
                           }}
                         >
@@ -265,7 +283,7 @@ function Landing() {
                           className="bg-white px-4 py-2 cursor-pointer text-sm hover:bg-gray-50 border border-lightBlue text-gray-800"
                           onClick={(e) => {
                             setOpenCarreras(false);
-                            setCarrera("Big_Data_y_Ciencia_de_Datos");
+                            setCarrera('Big_Data_y_Ciencia_de_Datos');
                           }}
                         >
                           Big Data y Ciencia de Datos
@@ -275,7 +293,7 @@ function Landing() {
                           onClick={(e) => {
                             setOpenCarreras(false);
                             setCarrera(
-                              "Diseño_y_Desarrollo_de_Simuladores_y_Videojuegos"
+                              'Diseño_y_Desarrollo_de_Simuladores_y_Videojuegos'
                             );
                           }}
                         >
@@ -285,7 +303,7 @@ function Landing() {
                           className="bg-white px-4 py-2 cursor-pointer text-sm hover:bg-gray-50 border border-lightBlue text-gray-800"
                           onClick={(e) => {
                             setOpenCarreras(false);
-                            setCarrera("Diseño_y_Desarrollo_de_Software");
+                            setCarrera('Diseño_y_Desarrollo_de_Software');
                           }}
                         >
                           Diseño y Desarrollo de Software
@@ -294,7 +312,7 @@ function Landing() {
                           className="bg-white px-4 py-2 cursor-pointer text-sm hover:bg-gray-50 border border-lightBlue text-gray-800"
                           onClick={(e) => {
                             setOpenCarreras(false);
-                            setCarrera("Modelado_y_Animación_Digital");
+                            setCarrera('Modelado_y_Animación_Digital');
                           }}
                         >
                           Modelado y Animación Digital
@@ -303,7 +321,7 @@ function Landing() {
                           className="bg-white px-4 py-2 cursor-pointer text-sm hover:bg-gray-50 border border-lightBlue text-gray-800"
                           onClick={(e) => {
                             setOpenCarreras(false);
-                            setCarrera("Operaciones_Mineras");
+                            setCarrera('Operaciones_Mineras');
                           }}
                         >
                           Operaciones Mineras
@@ -312,7 +330,7 @@ function Landing() {
                           className="bg-white px-4 py-2 cursor-pointer text-sm hover:bg-gray-50 border border-lightBlue text-gray-800"
                           onClick={(e) => {
                             setOpenCarreras(false);
-                            setCarrera("Procesos_Químicos_y_Metalúrgicos");
+                            setCarrera('Procesos_Químicos_y_Metalúrgicos');
                           }}
                         >
                           Procesos Químicos y Metalúrgicos
@@ -321,7 +339,7 @@ function Landing() {
                           className="bg-white px-4 py-2 cursor-pointer text-sm hover:bg-gray-50 border border-lightBlue text-gray-800"
                           onClick={(e) => {
                             setOpenCarreras(false);
-                            setCarrera("Producción_y_Gestión_Industrial");
+                            setCarrera('Producción_y_Gestión_Industrial');
                           }}
                         >
                           Producción y Gestión Industrial
@@ -330,7 +348,7 @@ function Landing() {
                           className="bg-white px-4 py-2 cursor-pointer text-sm hover:bg-gray-50 border border-lightBlue text-gray-800"
                           onClick={(e) => {
                             setOpenCarreras(false);
-                            setCarrera("Logística_Digital_Integrada");
+                            setCarrera('Logística_Digital_Integrada');
                           }}
                         >
                           Logística Digital Integrada
@@ -339,16 +357,16 @@ function Landing() {
                           className="bg-white px-4 py-2 cursor-pointer text-sm hover:bg-gray-50 border border-lightBlue text-gray-800"
                           onClick={(e) => {
                             setOpenCarreras(false);
-                            setCarrera("Diseño_Industrial");
+                            setCarrera('Diseño_Industrial');
                           }}
                         >
-                          Diseño Industrial{" "}
+                          Diseño Industrial{' '}
                         </div>
                         <div
                           className="bg-white px-4 py-2 cursor-pointer text-sm hover:bg-gray-50 border border-lightBlue text-gray-800"
                           onClick={(e) => {
                             setOpenCarreras(false);
-                            setCarrera("Aviónica_y_Mecánica_Aeronáutica");
+                            setCarrera('Aviónica_y_Mecánica_Aeronáutica');
                           }}
                         >
                           Aviónica y Mecánica Aeronáutica
@@ -358,7 +376,7 @@ function Landing() {
                           onClick={(e) => {
                             setOpenCarreras(false);
                             setCarrera(
-                              "Gestión_y_Mantenimiento_de_Maquinaria_Industrial"
+                              'Gestión_y_Mantenimiento_de_Maquinaria_Industrial'
                             );
                           }}
                         >
@@ -369,7 +387,7 @@ function Landing() {
                           onClick={(e) => {
                             setOpenCarreras(false);
                             setCarrera(
-                              "Gestión_y_Mantenimiento_de_Maquinaria_Pesada"
+                              'Gestión_y_Mantenimiento_de_Maquinaria_Pesada'
                             );
                           }}
                         >
@@ -379,7 +397,7 @@ function Landing() {
                           className="bg-white px-4 py-2 cursor-pointer text-sm hover:bg-gray-50 border border-lightBlue text-gray-800"
                           onClick={(e) => {
                             setOpenCarreras(false);
-                            setCarrera("Mantenimiento_de_Equipo_Pesado");
+                            setCarrera('Mantenimiento_de_Equipo_Pesado');
                           }}
                         >
                           Mantenimiento de Equipo Pesado
@@ -388,7 +406,7 @@ function Landing() {
                           className="bg-white px-4 py-2 cursor-pointer text-sm hover:bg-gray-50 border border-lightBlue text-gray-800"
                           onClick={(e) => {
                             setOpenCarreras(false);
-                            setCarrera("Mecatrónica_Industrial");
+                            setCarrera('Mecatrónica_Industrial');
                           }}
                         >
                           Mecatrónica Industrial
@@ -397,7 +415,7 @@ function Landing() {
                           className="bg-white px-4 py-2 cursor-pointer text-sm hover:bg-gray-50 border border-lightBlue text-gray-800"
                           onClick={(e) => {
                             setOpenCarreras(false);
-                            setCarrera("Electricidad_Industrial");
+                            setCarrera('Electricidad_Industrial');
                           }}
                         >
                           Electricidad Industrial
@@ -407,7 +425,7 @@ function Landing() {
                           onClick={(e) => {
                             setOpenCarreras(false);
                             setCarrera(
-                              "Electrónica_y_Automatización_Industrial"
+                              'Electrónica_y_Automatización_Industrial'
                             );
                           }}
                         >
@@ -429,7 +447,7 @@ function Landing() {
                         <div
                           className="bg-white px-4 py-2 cursor-pointer text-sm hover:bg-gray-50 border border-lightBlue text-gray-800"
                           onClick={() => {
-                            setModalidad("Alumno_Talento");
+                            setModalidad('Alumno_Talento');
                             setOpenModalidad(false);
                           }}
                         >
@@ -438,7 +456,7 @@ function Landing() {
                         <div
                           className="bg-white px-4 py-2 cursor-pointer text-sm hover:bg-gray-50 border border-lightBlue text-gray-800"
                           onClick={() => {
-                            setModalidad("Examen_de_Admisión");
+                            setModalidad('Examen_de_Admisión');
                             setOpenModalidad(false);
                           }}
                         >
@@ -447,7 +465,7 @@ function Landing() {
                         <div
                           className="bg-white px-4 py-2 cursor-pointer text-sm hover:bg-gray-50 border border-lightBlue text-gray-800"
                           onClick={() => {
-                            setModalidad("Tec_Gym");
+                            setModalidad('Tec_Gym');
                             setOpenModalidad(false);
                           }}
                         >
@@ -467,7 +485,7 @@ function Landing() {
                     className="w-4 h-4 bg-pink-300 checked:bg-rose-500 cursor-pointer"
                   />
                   <label className="ml-2 text-gray-400 text-xs avenir m-auto items-center">
-                    He leído y acepto las{" "}
+                    He leído y acepto las{' '}
                     <span
                       className="cursor-pointer hover:underline hover:font-bold"
                       onClick={() => saveFile()}
@@ -476,11 +494,17 @@ function Landing() {
                     </span>
                   </label>
                 </div>
+                {messageForm && (
+                  <div className="text-center text-red text-xs">
+                    {messageForm}
+                  </div>
+                )}
+                {}
                 <button
                   type="submit"
                   className="sm:w-52 m-auto bg-lightBlue text-white font-bold py-2 px-4 rounded w-full"
                 >
-                  <Link to="/gracias">Inscríbete aquí</Link>
+                  Inscríbete aquí
                 </button>
               </form>
             </div>
