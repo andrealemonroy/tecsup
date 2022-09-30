7536f9218910b38a1fb8bb621ef0968e0f174774
luismonroy1971 <34591655+luismonroy1971@users.noreply.github.com>
2022-09-29 20:47:56 -0500

    Formulario inyectado

--- src/components/Formulario/Formulario.jsx
+++ src/components/Formulario/Formulario.jsx
@@ -7,7 +7,7 @@ const Formulario = () => {
         id="inlineFrameExample"
         title="Inline Frame Example"
         width="100%"
-        height="610px"
+        height="550px"
         src="https://www.tecsup.edu.pe/landings/crmtivenos.html?unidad=Pregrado&familia=Formaci%C3%B3n%20Regular&sede=Lima&proceso=PROCESO%20DE%20ADMISION%202023-1"
       ></iframe>
     </div>
