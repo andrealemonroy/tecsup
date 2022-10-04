414ab8931b2c0fe42655175e641f5136baa26e2c
luismonroy1971 <34591655+luismonroy1971@users.noreply.github.com>
2022-10-04 15:00:31 -0500

    Formulario corregido

--- src/components/Formulario/Formulario.jsx
+++ src/components/Formulario/Formulario.jsx
@@ -1,6 +1,20 @@
 import React from "react";
 
 const Formulario = () => {
+  const unidad = "Pregrado";
+  const familia = "Formaci%C3%B3n%20Regular";
+  const sede = "Lima";
+  const proceso = "PROCESO%20DE%20ADMISION%202023-1";
+  const source =
+    "https://www.tecsup.edu.pe/landings/crmtivenos.html?unidad=" +
+    unidad +
+    "&familia=" +
+    familia +
+    "&sede=" +
+    sede +
+    "&proceso=" +
+    proceso;
+  console.log(source);
   return (
     <div>
       <iframe
@@ -8,7 +22,7 @@ const Formulario = () => {
         title="Inline Frame Example"
         width="100%"
         height="550px"
-        src="https://www.tecsup.edu.pe/landings/crmtivenos.html?unidad=Pregrado&familia=Formaci%C3%B3n%20Regular&sede=Lima&proceso=PROCESO%20DE%20ADMISION%202023-1"
+        src={source}
       ></iframe>
     </div>
   );
